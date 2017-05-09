eca514a7d1cbbb61c4c79166751190aaf2f04e95
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-05-09 14:37:59 +0100

    Adds hangar variable to h Airport to hold landed Plane object

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,12 +1,19 @@
 describe ('Airport', function() {
   var airPort;
+  var plane;
 
   beforeEach(function() {
     airPort = new AirPort();
+    plane = new Plane();
   });
 
   it ('Confirms plane has landed', function() {
-    expect(airPort.landed('plane 1')).toBe('Landing confirmed');
+    expect(airPort.land(plane)).toBe('Landing confirmed');
+  });
+
+  it ('Stores list of plane at airport', function () {
+    airPort.land(plane);
+    expect(airPort.hangar).toContain(plane);
   });
 
 });
--- src/Airport.js
+++ src/Airport.js
@@ -1,9 +1,8 @@
-
 function AirPort() {
-
+  this.hangar = [];
 }
 
-AirPort.prototype.landed = function(plane) {
+AirPort.prototype.land = function(plane) {
+  (this.hangar).push(plane);
   return('Landing confirmed');
-
 };
